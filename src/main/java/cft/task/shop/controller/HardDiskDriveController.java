12aaaa36a329af2b80e5532f6e38646dd11dffc2
Vadim <vadiman158@gmail.com>
2021-09-29 17:12:18 +0700

    Added methods for create, update, read and get hard disk drives in HardDiskDriveController.

--- src/main/java/cft/task/shop/controller/HardDiskDriveController.java
+++ src/main/java/cft/task/shop/controller/HardDiskDriveController.java
@@ -1,7 +1,38 @@
 package cft.task.shop.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import cft.task.shop.model.HardDiskDrive;
+import cft.task.shop.service.HardDiskDriveService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class HardDiskDriveController {
+    private final HardDiskDriveService hardDiskDriveService;
+
+    public HardDiskDriveController(HardDiskDriveService hardDiskDriveService) {
+        this.hardDiskDriveService = hardDiskDriveService;
+    }
+
+    @PostMapping("/disk")
+    public ResponseEntity<?> create(@RequestBody HardDiskDrive hdd) {
+        return ResponseEntity.ok(hardDiskDriveService.create(hdd));
+    }
+
+    @PostMapping("/disk/{diskId}")
+    public ResponseEntity<?> update(@PathVariable(name = "diskId") int diskId,
+                                    HardDiskDrive disk) {
+        return ResponseEntity.ok(hardDiskDriveService.update(diskId, disk));
+    }
+
+    @GetMapping("/disks")
+    public List<HardDiskDrive> read() {
+        return hardDiskDriveService.getAllHardDiskDrives();
+    }
+
+    @GetMapping("/disk/{diskId}")
+    public ResponseEntity<?> get(@PathVariable(name = "diskId") int diskId) {
+        return ResponseEntity.ok(hardDiskDriveService.getDesktopComputer());
+    }
 }

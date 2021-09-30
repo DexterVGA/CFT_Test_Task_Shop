9dd891477f4c603676511d700b1be59d88ca10ff
Vadim <vadiman158@gmail.com>
2021-09-30 20:05:32 +0700

    Added validation for LaptopController.

--- src/main/java/cft/task/shop/controller/LaptopController.java
+++ src/main/java/cft/task/shop/controller/LaptopController.java
@@ -1,5 +1,7 @@
 package cft.task.shop.controller;
 
+import cft.task.shop.error.ErrorHandler;
+import cft.task.shop.error.ValidationResult;
 import cft.task.shop.model.Laptop;
 import cft.task.shop.service.LaptopService;
 import org.springframework.http.ResponseEntity;
@@ -17,13 +19,30 @@ public class LaptopController {
 
     @PostMapping("/laptop")
     public ResponseEntity<?> create(@RequestBody Laptop laptop) {
+        ValidationResult returnedRequestStatus = ErrorHandler.validateLaptop(laptop);
+        if (returnedRequestStatus != ValidationResult.NO_ERROR) {
+            return ResponseEntity.internalServerError().body(returnedRequestStatus);
+        }
         return ResponseEntity.ok(laptopService.create(laptop));
     }
 
     @PutMapping("/laptop/{laptopId}")
     public ResponseEntity<?> update(@PathVariable(name = "laptopId") int laptopId,
-                                    @RequestBody Laptop laptop) throws Exception {
-        return ResponseEntity.ok(laptopService.update(laptop, laptopId));
+                                    @RequestBody Laptop laptop) {
+        ResponseEntity<?> response;
+        ValidationResult returnedRequestStatus = ErrorHandler.validateLaptop(laptop);
+        if (returnedRequestStatus == ValidationResult.NO_ERROR) {
+            Laptop returnedLaptop;
+            try {
+                returnedLaptop = laptopService.update(laptop, laptopId);
+                response = ResponseEntity.ok(returnedLaptop);
+            } catch (Exception e) {
+                response = ResponseEntity.internalServerError().body(ValidationResult.LAPTOP_NOT_FOUND);
+            }
+        } else {
+            response = ResponseEntity.internalServerError().body(returnedRequestStatus);
+        }
+        return response;
     }
 
     @GetMapping("/laptops")
@@ -33,6 +52,12 @@ public class LaptopController {
 
     @GetMapping("/laptop/{laptopId}")
     public ResponseEntity<?> get(@PathVariable(name = "laptopId") int laptopId) {
-        return ResponseEntity.ok(laptopService.getLaptop(laptopId));
+        Laptop returnedLaptop;
+        try {
+            returnedLaptop = laptopService.getLaptop(laptopId);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body(ValidationResult.LAPTOP_NOT_FOUND);
+        }
+        return ResponseEntity.ok(returnedLaptop);
     }
 }

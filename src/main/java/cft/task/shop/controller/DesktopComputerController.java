539af6a30267f6af2f556d3e93edb7478d08e46c
Vadim <vadiman158@gmail.com>
2021-09-30 19:32:12 +0700

    Added validation for DesktopComputerController. Added new fields in ValidationResult.

--- src/main/java/cft/task/shop/controller/DesktopComputerController.java
+++ src/main/java/cft/task/shop/controller/DesktopComputerController.java
@@ -1,5 +1,7 @@
 package cft.task.shop.controller;
 
+import cft.task.shop.error.ErrorHandler;
+import cft.task.shop.error.ValidationResult;
 import cft.task.shop.model.DesktopComputer;
 import cft.task.shop.service.DesktopComputerService;
 import org.springframework.http.ResponseEntity;
@@ -17,13 +19,30 @@ public class DesktopComputerController {
 
     @PostMapping("/desktop")
     public ResponseEntity<?> create(@RequestBody DesktopComputer desktopComputer) {
+        ValidationResult returnedRequestStatus = ErrorHandler.validateDesktopComputer(desktopComputer);
+        if (returnedRequestStatus != ValidationResult.NO_ERROR) {
+            return ResponseEntity.internalServerError().body(returnedRequestStatus);
+        }
         return ResponseEntity.ok(desktopComputerService.create(desktopComputer));
     }
 
     @PutMapping("/desktop/{desktopId}")
     public ResponseEntity<?> update(@PathVariable(name = "desktopId") int desktopId,
-                                    @RequestBody DesktopComputer desktopComputer) throws Exception {
-        return ResponseEntity.ok(desktopComputerService.update(desktopComputer, desktopId));
+                                    @RequestBody DesktopComputer desktopComputer) {
+        ResponseEntity<?> response;
+        ValidationResult returnedRequestStatus = ErrorHandler.validateDesktopComputer(desktopComputer);
+        if (returnedRequestStatus == ValidationResult.NO_ERROR) {
+            DesktopComputer returnedComputer;
+            try {
+                returnedComputer = desktopComputerService.update(desktopComputer, desktopId);
+                response = ResponseEntity.ok(returnedComputer);
+            } catch (Exception e) {
+                response = ResponseEntity.internalServerError().body(ValidationResult.DESKTOP_COMPUTER_NOT_FOUND);
+            }
+        } else {
+            response = ResponseEntity.internalServerError().body(returnedRequestStatus);
+        }
+        return response;
     }
 
     @GetMapping("/desktops")
@@ -33,6 +52,12 @@ public class DesktopComputerController {
 
     @GetMapping("/desktop/{desktopId}")
     public ResponseEntity<?> get(@PathVariable(name = "desktopId") int desktopId) {
-        return ResponseEntity.ok(desktopComputerService.getDesktopComputer(desktopId));
+        DesktopComputer returnedComputer;
+        try {
+            returnedComputer = desktopComputerService.getDesktopComputer(desktopId);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body(ValidationResult.DESKTOP_COMPUTER_NOT_FOUND);
+        }
+        return ResponseEntity.ok(returnedComputer);
     }
 }

daa058fa5ecde2301a179bcffac16df0d32a3e3c
Vadim <vadiman158@gmail.com>
2021-09-30 19:08:56 +0700

    Added validation for input data.

---
+++ src/main/java/cft/task/shop/error/ErrorHandler.java
@@ -0,0 +1,118 @@
+package cft.task.shop.error;
+
+import cft.task.shop.model.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(Throwable.class)
+    @ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public ValidationResult handleAnyException(Throwable exception) {
+        return ValidationResult.UNKNOWN_ERROR;
+    }
+
+    public static ValidationResult validateProduct(Product someProduct) {
+        if (someProduct.getSerialNumber() == 0 && someProduct.getProducer() == null &&
+                someProduct.getPrice() == 0 && someProduct.getNumberProductUnitsInStock() == 0) {
+            return ValidationResult.ALL_FIELDS_ARE_EMPTY;
+        } else if (someProduct.getSerialNumber() == 0) {
+            return ValidationResult.SERIAL_NUMBER_IS_EMPTY;
+        } else if (someProduct.getProducer() == null) {
+            return ValidationResult.PRODUCER_IS_EMPTY;
+        } else if (someProduct.getPrice() == 0) {
+            return ValidationResult.PRICE_IS_EMPTY;
+        } else if (someProduct.getNumberProductUnitsInStock() == 0) {
+            return ValidationResult.NUMBER_PRODUCT_UNITS_IN_STOCK_IS_EMPTY;
+        }
+        return ValidationResult.NO_ERROR;
+    }
+
+    public static ValidationResult validateDesktopComputer(DesktopComputer desktopComputer) {
+        ValidationResult validatePersonStatus = validateProduct(desktopComputer);
+        if (validatePersonStatus == ValidationResult.NO_ERROR) {
+            if (desktopComputer.getFormFactor() == null) {
+                return ValidationResult.FORM_FACTOR_IS_EMPTY;
+            } else if (!desktopComputer.getFormFactor().equals("десктоп") ||
+                    !desktopComputer.getFormFactor().equals("неттоп") ||
+                    !desktopComputer.getFormFactor().equals("моноблок")) {
+                return ValidationResult.FORM_FACTOR_IS_WRONG;
+            }
+        } else if (validatePersonStatus == ValidationResult.ALL_FIELDS_ARE_EMPTY) {
+            if (desktopComputer.getFormFactor() == null) {
+                return ValidationResult.ALL_FIELDS_ARE_EMPTY;
+            } else {
+                return ValidationResult.SOME_FIELDS_ARE_EMPTY;
+            }
+        } else {
+            return validatePersonStatus;
+        }
+        return ValidationResult.NO_ERROR;
+    }
+
+    public static ValidationResult validateHardDiskDrive(HardDiskDrive hardDisk) {
+        ValidationResult validatePersonStatus = validateProduct(hardDisk);
+        if (validatePersonStatus == ValidationResult.NO_ERROR) {
+            if (hardDisk.getMemorySizeGB() == 0) {
+                return ValidationResult.MEMORY_SIZE_IS_EMPTY;
+            } else if (hardDisk.getMemorySizeGB() < 0) {
+                return ValidationResult.MEMORY_SIZE_LESS_THAN_ZERO;
+            }
+        } else if (validatePersonStatus == ValidationResult.ALL_FIELDS_ARE_EMPTY) {
+            if (hardDisk.getMemorySizeGB() == 0) {
+                return ValidationResult.ALL_FIELDS_ARE_EMPTY;
+            } else {
+                return ValidationResult.SOME_FIELDS_ARE_EMPTY;
+            }
+        } else {
+            return validatePersonStatus;
+        }
+        return ValidationResult.NO_ERROR;
+    }
+
+    public static ValidationResult validateLaptop(Laptop laptop) {
+        ValidationResult validatePersonStatus = validateProduct(laptop);
+        if (validatePersonStatus == ValidationResult.NO_ERROR) {
+            if (laptop.getSizeInInches() == 0) {
+                return ValidationResult.LAPTOP_SIZE_IS_EMPTY;
+            } else if (laptop.getSizeInInches() != 13 || laptop.getSizeInInches() != 14 ||
+                    laptop.getSizeInInches() != 15 || laptop.getSizeInInches() != 17) {
+                return ValidationResult.LAPTOP_SIZE_IS_WRONG;
+            }
+        } else if (validatePersonStatus == ValidationResult.ALL_FIELDS_ARE_EMPTY) {
+            if (laptop.getSizeInInches() == 0) {
+                return ValidationResult.ALL_FIELDS_ARE_EMPTY;
+            } else {
+                return ValidationResult.SOME_FIELDS_ARE_EMPTY;
+            }
+        } else {
+            return validatePersonStatus;
+        }
+        return ValidationResult.NO_ERROR;
+    }
+
+    public static ValidationResult validateMonitor(Monitor monitor) {
+        ValidationResult validatePersonStatus = validateProduct(monitor);
+        if (validatePersonStatus == ValidationResult.NO_ERROR) {
+            if (monitor.getDiagonal() == 0) {
+                return ValidationResult.DIAGONAL_IS_EMPTY;
+            } else if (monitor.getDiagonal() < 0) {
+                return ValidationResult.DIAGONAL_LESS_THAN_ZERO;
+            }
+        } else if (validatePersonStatus == ValidationResult.ALL_FIELDS_ARE_EMPTY) {
+            if (monitor.getDiagonal() == 0) {
+                return ValidationResult.ALL_FIELDS_ARE_EMPTY;
+            } else {
+                return ValidationResult.SOME_FIELDS_ARE_EMPTY;
+            }
+        } else {
+            return validatePersonStatus;
+        }
+        return ValidationResult.NO_ERROR;
+    }
+}
---
+++ src/main/java/cft/task/shop/error/ValidationResult.java
@@ -0,0 +1,30 @@
+package cft.task.shop.error;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+public enum ValidationResult {
+    ALL_FIELDS_ARE_EMPTY,
+    SOME_FIELDS_ARE_EMPTY,
+    SERIAL_NUMBER_IS_EMPTY,
+    PRODUCER_IS_EMPTY,
+    PRICE_IS_EMPTY,
+    NUMBER_PRODUCT_UNITS_IN_STOCK_IS_EMPTY,
+    FORM_FACTOR_IS_EMPTY,
+    FORM_FACTOR_IS_WRONG,
+    MEMORY_SIZE_IS_EMPTY,
+    MEMORY_SIZE_LESS_THAN_ZERO,
+    LAPTOP_SIZE_IS_EMPTY,
+    LAPTOP_SIZE_IS_WRONG,
+    DIAGONAL_IS_EMPTY,
+    DIAGONAL_LESS_THAN_ZERO,
+    UNKNOWN_ERROR,
+    NO_ERROR,
+    ;
+
+    @JsonSerialize
+    String getCode() {
+        return name();
+    }
+}
